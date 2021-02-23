a0c80b83f17b3397ffd5551777ba1fb2704066cc
EC2 Default User <ec2-user@ip-172-31-42-1.us-east-2.compute.internal>
2021-02-23 19:54:48 +0000

    Part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,13 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    if !params.has_key?(:ratings)
+      @ratings_to_show=[]
+    else
+      @ratings_to_show= params[:ratings].keys
+    end
+    @movies = Movie.with_ratings(@ratings_to_show)
   end
 
   def new
